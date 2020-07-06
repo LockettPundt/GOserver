6c9a0064334f88477512329f2d12e648d02e6cea
Lockett Pundt <kalifornium@hotmail.com>
2020-07-05 20:18:30 -0400

    imported utility package and testedd the env variable function for the DBname and URI.

--- server.go
+++ server.go
@@ -6,12 +6,19 @@ import (
 	"net/http"
 
 	routes "github.com/LockettPundt/GOserver/routes"
+	utils "github.com/LockettPundt/GOserver/utils"
 	"github.com/slayer/autorestart"
 )
 
 func main() {
 	autorestart.StartWatcher()
 	fileServer := http.FileServer(http.Dir("./static"))
+
+	dbName := utils.GoDotEnvVariable("DB_NAME")
+	db_URI := utils.GoDotEnvVariable("DB_URI")
+
+	fmt.Println(dbName)
+	fmt.Println(db_URI)
 
 	http.Handle("/", fileServer)
 	http.HandleFunc("/form", routes.FormHandler)
