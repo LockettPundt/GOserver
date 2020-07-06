10c150a7d41acf6b41e94aa3924cc19ece65ae81
Lockett Pundt <kalifornium@hotmail.com>
2020-07-06 11:29:32 -0400

    imported the movie model package and put the AddMovie in the formhandler controller. it takes the incoming request and passes it into AddMovie() that updates the DB with the movie.

--- routes/routes.go
+++ routes/routes.go
@@ -3,8 +3,11 @@ package routes
 import (
 	"fmt"
 	"net/http"
+
+	models "github.com/LockettPundt/GOserver/models"
 )
 
+// HelloHandler is the controller for the /hello route
 func HelloHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/hello" {
 		http.Error(w, "404 not found.", http.StatusNotFound)
@@ -19,15 +22,22 @@ func HelloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello!")
 }
 
+// FormHandler is the controller for the /form.html route
 func FormHandler(w http.ResponseWriter, r *http.Request) {
 	if err := r.ParseForm(); err != nil {
 		fmt.Fprintf(w, "ParseForm() err: %v", err)
 		return
 	}
 	fmt.Fprintf(w, "POST request successful")
-	name := r.FormValue("name")
-	address := r.FormValue("address")
+	title := r.FormValue("title")
+	releaseDate := r.FormValue("releaseDate")
+	rating := r.FormValue("rating")
+	status := r.FormValue("status")
+	models.AddMovie(title, releaseDate, rating, status)
 
-	fmt.Fprintf(w, "Name = %s\n", name)
-	fmt.Fprintf(w, "Address = %s\n", address)
+	fmt.Fprintf(w, "Title = %s\n", title)
+	fmt.Fprintf(w, "date = %s\n", releaseDate)
+	fmt.Fprintf(w, "rating = %T\n", rating)
+	fmt.Fprintf(w, "status = %s\n", status)
+
 }
